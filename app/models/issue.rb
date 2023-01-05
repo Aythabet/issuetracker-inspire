46f0fe81fb5bc9668c2489fc0ad3ebedc456bd05
Ayoub Ben Thabet <ayoubthabet@gmail.com>
2023-01-05 09:59:39 +0100

    update style add new issue form

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,14 +1,14 @@
 class Issue < ApplicationRecord
   max_paginates_per 10
 
-  before_validation :check_project_exists
 	before_create { |issue| issue.jiraid = issue.jiraid.upcase }
 
 
 	validates :jiraid, uniqueness: true
 
 	validates :jiraid, :project, :owner, :departement, :time_forecast, :time_real, presence: true
-  	validates :jiraid, format: { with: /\b[a-zA-Z]{2,4}-[1-9]\d{0,3}\b/, message: "must follow this format AAAA-9999" }
+  validates :jiraid, format: { with: /\b[a-zA-Z]{2,4}-[1-9]\d{0,3}\b/, message: "must follow this format AAAA-9999" }
+  before_validation :check_project_exists
 
   	validates :time_real, numericality: { only_float: true }
   	validates :time_forecast, numericality: { only_float: true }
@@ -16,7 +16,6 @@ class Issue < ApplicationRecord
 
   def check_project_exists
     first_part = jiraid.upcase.match(/\b[a-zA-Z]{2,4}/)[0]
-    p(jiraid)
     unless Project.exists?(jiraid: first_part)
       errors.add(:jiraid, "does not belong to a valid project")
     end
