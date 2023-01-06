9f68b40f87380125376d9e61e9e35589470d45d4
Ayoub Ben Thabet <ayoubthabet@gmail.com>
2023-01-06 15:27:49 +0100

    devise working, user signup login config and navbar links updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get "users", to: "devise/sessions#new"
+  end
+
+  devise_for :users
   get '/search', to: "issues#search"
   get '/home/index'
   root "projects#index"
