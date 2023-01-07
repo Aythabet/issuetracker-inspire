3f9ceded8e98857f779d3c956ea41afc2a522f9b
Ayoub Ben Thabet <ayoubthabet@gmail.com>
2023-01-07 10:54:56 +0100

    permissions updated on all project

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,22 @@
 Rails.application.routes.draw do
-  devise_scope :user do
-    get "users", to: "devise/sessions#new"
-  end
 
-  devise_for :users
   get '/search', to: "issues#search"
   get '/home/index'
-  root "projects#index"
 
   resources :projects
   resources :issues
   resources :owners
   resources :departements
+
+  # Devise routes
+  devise_scope :user do
+    get "users", to: "devise/sessions#new"
+  end
+
+  devise_for :users
+  authenticated :user do
+    root to: 'projects#index', as: :authenticated_root
+  end
+  root to: redirect('/users/sign_in')
+
 end
