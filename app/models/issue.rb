817e7dd2617648a22cbb614965261f9dc06d8396
Ayoub Ben Thabet <ayoubthabet@gmail.com>
2023-01-11 21:26:16 +0100

    updated message and hid the delete project button

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -5,22 +5,20 @@ class Issue < ApplicationRecord
 
   before_create { |issue| issue.jiraid = issue.jiraid.upcase }
 
-
   validates :jiraid, uniqueness: true
 
   validates :jiraid, :project, :owner, :departement, :time_forecast, :time_real, presence: true
-  validates :jiraid, format: { with: /\b[a-zA-Z]{2,4}-[1-9]\d{0,3}\b/, message: "must follow this format ABCXYZ-9999" }
+  validates :jiraid, format: { with: /\b[a-zA-Z]{2,4}-[1-9]\d{0,3}\b/, message: 'must follow this format ABCXYZ-9999' }
   before_validation :check_project_exists
   before_validation :check_jiraid_project_match
 
   validates :time_real, numericality: { only_float: true }
   validates :time_forecast, numericality: { only_float: true }
 
-
   def check_project_exists
     first_part = jiraid.upcase.match(/\b[a-zA-Z]{2,4}/)[0]
     unless Project.exists?(jiraid: first_part)
-      errors.add(:jiraid, "does not belong to a valid project")
+      errors.add(:jiraid, 'does not belong to a valid project')
     end
   end
 
@@ -28,8 +26,7 @@ class Issue < ApplicationRecord
     first_part = jiraid.upcase.match(/\b[a-zA-Z]{2,4}/)[0]
     reference_project_jiraid = Project.find_by(id: project_id).jiraid
     unless first_part == reference_project_jiraid
-      errors.add(:jiraid, "Please verify that you have the right project")
+      errors.add(:jiraid, 'Please verify that you selected the correct project')
     end
   end
-
 end
