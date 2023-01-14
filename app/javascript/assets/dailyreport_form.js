62cee729b9d4d148f2c82373c124819e99b22219
Ayoub Ben Thabet <ayoubthabet@gmail.com>
2023-01-14 12:18:50 +0100

    Added add and remove issues, still not working. Updated General style of the form

--- app/javascript/assets/dailyreport_form.js
+++ app/javascript/assets/dailyreport_form.js
@@ -1,13 +1,8 @@
-// Add and remove issue fields in the daily report form
-$(document).on("click", ".add_fields", function(e) {
-  e.preventDefault();
-  var time = new Date().getTime();
-  var regexp = new RegExp($(this).data("id"), "g");
-  var newFields = $("#issues_fields").html();
-$(this).before(newFields);
-});
-
-$(document).on("click", ".remove_fields", function(e) {
-  e.preventDefault();
-  $(this).closest(".nested-fields").remove();
-});
+  $(document).on("click", ".add-issue", function(){
+    var newIssue = $(".issues-fields:last").clone();
+    newIssue.find("input").val("");
+    newIssue.appendTo(".issues-fields-container");
+  });
+  $(document).on("click", ".remove-issue", function(){
+    $(this).closest(".issues-fields").remove();
+  });
