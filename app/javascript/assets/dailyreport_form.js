5287e61f559ded99877f5ef1eb6c5f205a13b09f
Ayoub Ben Thabet <ayoubthabet@gmail.com>
2023-01-16 21:11:55 +0100

    Add and remove issue fields working visually
    The JS behavior is working.
    Still the controller actions and treating the sceario of only one issue and the remove button

--- app/javascript/assets/dailyreport_form.js
+++
@@ -1,8 +0,0 @@
-  $(document).on("click", ".add-issue", function(){
-    var newIssue = $(".issues-fields:last").clone();
-    newIssue.find("input").val("");
-    newIssue.appendTo(".issues-fields-container");
-  });
-  $(document).on("click", ".remove-issue", function(){
-    $(this).closest(".issues-fields").remove();
-  });
